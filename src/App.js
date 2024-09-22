2ed2d8df103e30dbaaa863ed146c1ba55e7c4589
kocaman14 <163642815+kocaman14@users.noreply.github.com>
2024-09-22 13:35:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // Her bir butonun üzerine, içerikte yazan eyleme karşılık gelen satır içi/inline stiller ekleyin
 export default function ButtonVariants() {
   return (
-    <span className='wrapper flex space-x-2'>
-      <button className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700'>
+  <span className='wrapper'>
+        <button className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700'>
         Create
       </button>
       <button className='bg-yellow-500 text-white py-2 px-4 rounded hover:bg-yellow-700'>
